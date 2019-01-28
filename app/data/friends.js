ca1d791e6a095d6b3ebf85f334035f5a77c4c084
dpd1208 <dpd1208@gmail.com>
2019-01-27 22:41:54 -0700

    add friend

--- app/data/friends.js
+++ app/data/friends.js
@@ -5,19 +5,35 @@
 
 var userDataArray = [
     {
+        name: "Mike Tyson",
+        photo: "http://www.gstatic.com/tv/thumb/persons/1822/1822_v9_bb.jpg",
+        scores: [
+          5,
+          5,
+          5,
+          5,
+          5,
+          5,
+          5,
+          5,
+          5,
+          5
+        ],
+    },
+    {
         name: "Lisa Kudrow",
         photo: "https://pbs.twimg.com/profile_images/1297725502/images_400x400.jpg",
         scores: [
-          5,
           4,
-          3,
-          2,
-          1,
-          2,
-          3,
           4,
-          5,
-          5
+          4,
+          4,
+          4,
+          4,
+          4,
+          4,
+          4,
+          4
         ],
     },
     {
@@ -25,31 +41,31 @@ var userDataArray = [
         photo: "https://cdn1.thr.com/sites/default/files/imagecache/scale_crop_768_433/2017/10/gettyimages-655851880_-_h_2017.jpg",
         scores: [
           3,
-          4,
           3,
-          2,
-          5,
-          4,
           3,
-          4,
-          2,
-          5
+          3,
+          3,
+          3,
+          3,
+          3,
+          3,
+          3
         ],
     },
     {
         name: "Nancy Pelosi",
         photo: "https://s1.r29static.com//bin/entry/0da/720x864,80/2114988/image.jpg",
         scores: [
-          5,
-          1,
-          1,
           2,
-          1,
           2,
-          1,
-          1,
-          5,
-          1
+          2,
+          2,
+          2,
+          2,
+          2,
+          2,
+          2,
+          2
         ],
     },
     {
